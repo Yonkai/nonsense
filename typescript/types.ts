d5eb0affb9378832d907b8be3898100ab1b587c1
Frank Gunsch <fgunsch.webdev@gmail.com>
2020-02-13 23:42:41 -0500

    add types

--- typescript/types.ts
+++ typescript/types.ts
@@ -1,1 +1,3 @@
 let decimal: number = 6; let hex: number = 0xf00d; let binary: number = 0b1010; let octal: number = 0o744; 
+let notSure: any = 4; notSure = "maybe a string instead"; notSure = false; // okay, definitely a boolean
+enum Color {Red = 1, Green, Blue} let colorName: string = Color[2]; console.log(colorName); // Displays 'Green' as its value is 2 above 
